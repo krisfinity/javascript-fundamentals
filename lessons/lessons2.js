ce4bcf216c0f726efc835bf88f4b1c4e961336d0
unknown <you@example.com>
2024-04-01 13:58:14 -0500

    Update 1

---
+++ lessons/lessons2.js
@@ -0,0 +1,15 @@
+console.log("Hello world!");
+
+// My first comment.
+
+/* My multiple
+comments are here */
+
+let helloText = "Hello";
+console.log(helloText);
+
+helloText = "New hello";
+console.log(helloText);
+
+const numberOne = 1;
+console.log(numberOne);
