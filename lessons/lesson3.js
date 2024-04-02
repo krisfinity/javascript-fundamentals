671f2d4072a5664632b33d914d6ecc40b1b63480
krisfinity <avant.trill@gmail.com>
2024-04-01 22:55:02 -0500

    task1

---
+++ lessons/lesson3.js
@@ -0,0 +1,14 @@
+//Declare a string using single quote
+let felineType = 'Cat'
+let felineAction = "They meow quietly.";
+
+//Declare a string using double quotes
+let canineType = "Dog";
+let canineAction = "They bark loudly";
+
+/*Using bracket notation on a variable of string
+will output 1 characters, or undefined */
+console.log(canineType[3])
+console.log(felineAction[7])
+console/log(canineAction[4]);
+console.log(felineType[0]);
