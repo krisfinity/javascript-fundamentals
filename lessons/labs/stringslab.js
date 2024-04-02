0754949e8efa168dfd03c95944adc3534a534f8f
krisfinity <avant.trill@gmail.com>
2024-04-02 12:44:39 -0500

    Strings Lab

---
+++ lessons/labs/stringslab.js
@@ -0,0 +1,15 @@
+/*task 1 Declaring Strings*/
+
+let personType = "Kristen";
+let personAction = "Is learning to code";
+
+
+
+console.log (msg.includes();
+console.log (msg.at();
+console.log (msg.trim();
+console.log (msg.toLowerCase();
+
+
+/*task2*\
+
