539c622caf423d1913fb930ee90715dee11f3e9a
krisfinity <avant.trill@gmail.com>
2024-04-02 21:34:23 -0500

    StringLab.js

---
+++ lessons/labs/StringLab.js
@@ -0,0 +1,34 @@
+/*task 1 Declaring Strings*/
+
+const msg = " This statement has empty spaces. ";
+
+console.log(msg.includes('has'));
+console.log(msg.at (-8));
+console.log(msg.indexOf('a'));
+console.log(msg.toUpperCase(" This statement has empty spaces. "));
+console.log(msg.toLowerCase(" This statement has empty spaces. "));
+console.log(msg.slice(15, 20));
+console.log(msg.slice( 6, 26));
+
+let person = "Kristen"
+console.log(person[4]);
+
+let word = msg.split('');
+console.log(word[3]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*task2*/
