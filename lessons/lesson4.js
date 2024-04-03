71a3113c76a3af9bda9c30261445ab8ca3a06149
krisfinity <avant.trill@gmail.com>
2024-04-02 20:23:46 -0500

    Numbers

---
+++ lessons/lesson4.js
@@ -0,0 +1,38 @@
+// Numbers examples
+const value1 = 42;
+const value2 = 34.4;
+const value3 = -34.3;
+
+// 17 decimal point precision
+const value4 = 4.38492748263438648;
+// console.log(value4);
+
+// BigInt values
+const value5 = 38492748263438648n;
+const value6 = 256n;
+// console.log(value6);
+
+// Number systems
+let ex_binary = 0b11010; // binary number
+let ex_hexadecimal = 0x1a; // hexadecimal number
+let ex_octal = 0o32; // octal number
+// console.log(ex_binary);
+// console.log(ex_hexadecimal);
+// console.log(ex_octal);
+
+// Exponential literals
+// Exponents are considered 10 to the something power
+const aa = 0E-3; // 0
+const bb = 0E+5; // 0
+const cc = 4E2; // 400
+const dd = 234e-2; // 2.34
+const ee = 2e-3; // 0.002
+// console.log(ee);
+
+// Numeric separators - using underscores for readability
+let gg = 9_563.25;
+let hh = 0b1010_0001;
+let ii = 0xA0_B0_C0;
+// console.log(gg);
+// console.log(hh);
+// console.log(ii);
